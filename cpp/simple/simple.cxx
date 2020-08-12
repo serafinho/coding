4b9781c8acf2cd839c69e5f053efc4e2b7d8b197
Ralf Ratering <108012572@ge.com>
2020-08-12 14:35:55 +0200

    started list cleaning

--- cpp/simple/simple.cxx
+++ cpp/simple/simple.cxx
@@ -38,9 +38,9 @@ void runFizzBuzz()
  */
 string reverseString(const string& input)
 {
-    const int length  = input.length();
+    const size_t length  = input.length();
     stringstream out; 
-    for(int i=0; i<length; i++)
+    for(size_t i=0; i<length; i++)
     {
         out << input.at(length-i-1);
     }
@@ -48,12 +48,27 @@ string reverseString(const string& input)
 }
 
 
+/** 
+* Print a list
+*/
+void printList(const list<int>& l)
+{
+    for (auto const& elem : l)
+    {
+        cout << elem << ", ";
+    }
+    cout << endl;
+}
+
 /**
  * Remove all duplicates from a list 
  */
 list<int> removeDuplicates(const list<int> &input)
 {
-    
+    list<int> result = input;
+    result.sort();
+    result.unique();
+    return result;
 }
 
 int main()
@@ -63,6 +78,15 @@ int main()
 
     const string inputString = "HonkyDoodle";
     cout << inputString << " reversed: " << reverseString(inputString) << endl;
+
+    list<int> input = { 1, 2, 3, 4, 1, 3, 5, 2, 2, 1, 5, 8, 1 };
+    cout << "Input: ";
+    printList(input);
+
+    auto output = removeDuplicates(input);
+    cout << "Output: ";
+    printList(output);
+
 
     return 0;
 }
