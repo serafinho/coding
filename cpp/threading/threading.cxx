83d4dd38a6f0b4622905f0d0092a6c546c043ca7
Ralf Ratering <108012572@ge.com>
2020-08-20 12:56:55 +0200

    First threading

--- cpp/threading/threading.cxx
+++ cpp/threading/threading.cxx
@@ -1,8 +1,38 @@
 #include <iostream>
 
+#include <future>
+#include <thread>
+
 using namespace std;
+
+void asyncPrint100()
+{
+	for (int i = 0; i < 100; i++)
+	{
+		cout << i << ", ";
+	}
+}
+
+void asyncPrint300()
+{
+	for (int i = 200; i < 300; i++)
+	{
+		cout << i << ", ";
+	}
+}
 
 int main()
 {
 	cout << "Running threading tests...\n";
+
+	auto fut = async(asyncPrint100);
+
+	thread t(asyncPrint300);
+
+	for (int i = 100; i < 200; i++)
+	{
+		cout << i << ", ";
+	}
+
+	t.join();
 }
