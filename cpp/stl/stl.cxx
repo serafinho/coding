fd2e13832fd3b9d8cffe57ed0896a81297685cb0
Serafin Plum <serafin.plum@gmail.com>
2021-04-08 12:36:22 +0200

    added stl

---
+++ cpp/stl/stl.cxx
@@ -0,0 +1,17 @@
+#include <memory>
+#include <iostream>
+#include <vector>
+#include <exception>
+
+using namespace std;
+
+void testVector()
+{
+    
+}
+
+int main()
+{
+    testVector();
+    return 0;
+}
